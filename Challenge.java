981657fbc5b079ed95900b3f477a7f5c8bcaec3c
Rodrigo Avila <rodrigoavilag12@gmail.com>
2023-04-17 18:35:40 -0600

    Challenge accepted and finished

--- Challenge.java
+++ Challenge.java
@@ -7,91 +7,88 @@ public class Challenge {
         String assignDishes = "";
         String [] dishes2 = dishes.split("");
 
+        System.err.println("Customer index equals to E: " + dishes2[customer].equals("E"));
 
         int count = 0;
         if(customer == 0){
-            System.err.println("Customer is equals to 0");
-            count+=turnsForwards(dishes2, count, dishesOrder, assignDishes);
-            System.err.println("assignDishes: " + assignDishes + " vs dishesOrder: " + dishesOrder);
-            System.err.println(count);
-            return count;
+            return turnsForwards(dishes2, dishesOrder, assignDishes);
         }
 
         if(customer > 0 && !dishes2[customer].equals("E")){
             String [] checkBackwards = Arrays.copyOfRange(dishes2, 0, customer+1);
-            String [] checkFordwar = Arrays.copyOfRange(dishes2, customer, dishes2.length);
             Collections.reverse(Arrays.asList(checkBackwards));
-            System.err.println("Customer is grater than 0");
-            System.err.println("FIRST IF checkBackwards array: " + Arrays.toString(checkBackwards) + "checkForward array: " + Arrays.toString(checkFordwar) + "Count backwards: " + count);
-            count += turnsForwards(checkFordwar, 0, dishesOrder, assignDishes) + countToEBackwards(checkBackwards, 0);;
-            System.err.println("count" + count);
-            return count;
+            int countBackwards = countToEBackwards(checkBackwards, dishesOrder, assignDishes);
+            String [] checkFordward = Arrays.copyOfRange(dishes2, customer-countBackwards, dishes2.length);
+            return turnsForwards(checkFordward, dishesOrder, assignDishes) + countBackwards;
         } else if (dishes2[customer].equals("E")) {
             String [] checkFordwar = Arrays.copyOfRange(dishes2, customer, dishes2.length);
-            System.err.println("Customer is grater than 0");
-            System.err.println("checkBackwards array: " + 0 + "checkForward array: " + Arrays.toString(checkFordwar) + "Count backwards: " + count);
-            count += turnsForwards(checkFordwar, 0, dishesOrder, assignDishes);
+            count += turnsForwards(checkFordwar, dishesOrder, assignDishes);
         }
-        System.err.println("assignDishes: " + assignDishes + " vs dishesOrder: " + dishesOrder);
         System.err.println(count);
         return count;
     }
 
-    public static String overwriteCreate(String create, String dishesIteration){
-        if(!dishesIteration.equals("N")){
-            if(!create.contains("E") && dishesIteration.equals("E")){
+    public static String overwriteCreate(String dishesIteration,int eCount,int pCount,int dCount){
+        if(!dishesIteration.contains("N")){
+            if(dishesIteration.contains("E") && eCount == 1){
                 return "E";
-            } else if (!create.contains("P") && dishesIteration.equals("P")) {
+            } else if (dishesIteration.contains("P") && pCount == 1) {
                 return "P";
-            }else if(!create.contains("D") && dishesIteration.equals("D")){
+            }else if(dishesIteration.contains("D") && dCount == 1){
                 return "D";
             }
         }
         return "";
     }
 
-    public static int turnsForwards(String [] dishes2, int count, String dishesOrder, String assignDishes){
+    public static int turnsForwards(String [] dishes2, String dishesOrder, String assignDishes){
+        int count = 0;
+        int eCount = 0;
+        int pCount = 0;
+        int dCount = 0;
         for(String x : dishes2){
-            assignDishes+=overwriteCreate(assignDishes,x);
-            System.out.println("Dish: " + x + " Iteration: " + count);
-            System.out.println("assignDishes inside of turnsForwards before: " + assignDishes);
+            if(x.equals("E")){
+                eCount++;
+            } else if (x.equals("P")) {
+                pCount++;
+            } else if (x.equals("D")) {
+                dCount++;
+            }
+            assignDishes+=overwriteCreate(x,eCount,pCount,dCount);
             if(dishesOrder.equals(assignDishes)){
                 break;
             }
-            System.out.println("Adding: " + overwriteCreate(assignDishes,x));
-            System.out.println("assignDishes inside of turnsForwards after: " + assignDishes);
-
-            if(x.equals("E") && assignDishes.contains("E")){
-                count++;
-            } else if (x.equals("P") && assignDishes.contains("P")) {
-                count++;
-            }else if (x.equals("D") && assignDishes.contains("D")) {
-                count++;
-            }else if (x.equals("N")) {
-                count++;
-            }
-            System.out.println("_________________");
+            count++;
         }
-        System.err.println("Count :" + count);
+        System.err.println("Fordward Count : " + count);
         return count;
     }
 
-    public static int countToEBackwards(String [] checkBackwards, int count){
-        String create = "";
+    public static int countToEBackwards(String [] checkBackwards, String dishesOrder, String assignDishes){
+        int count = 0;
+        int eCount = 0;
+        int pCount = 0;
+        int dCount = 0;
         for(String x : checkBackwards){
-            create+=overwriteCreate(create,x);
-            if(create.contains("E")){
-                break;
+            if(x.equals("E")){
+                eCount++;
+            } else if (x.equals("P")) {
+                pCount++;
+            } else if (x.equals("D")) {
+                dCount++;
             }
-            System.out.println("Dish: " + x + " Iteration: " + count);
-            if(!x.equals("E") && !create.equals("E")){
-                count++;
+            assignDishes+=overwriteCreate(x,eCount,pCount,dCount);
+            if(x.equals("E")){
+                System.err.println("Backwards Count : " + count);
+                return count;
             }
+            count++;
         }
+        System.err.println("Backwards Count : " + count);
         return count;
     }
 
     public static void main(String[] args) {
-        calculateTurns(14,2,"ENNPPPNNDN");
+        System.err.println("Count from main Page: " + calculateTurns(14,3,"NNNEPNNPPPDE"));
     }
 }
